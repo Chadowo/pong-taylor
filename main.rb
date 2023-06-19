02c9762b1d2c32d009d34cdaf8261674a1ef6162
Chadowo <83732118+Chadowo@users.noreply.github.com>
2023-06-19 18:50:01 -0300

    Simplifying the game's structure

--- main.rb
+++ main.rb
@@ -1,5 +1,4 @@
 # Add the path ./vendor so we can easily require third party libraries.
-
 $: << './vendor'
 $: << './src'
 
@@ -12,60 +11,62 @@ module Game
   attr_reader :player, :rival, :ball
 
   extend self
-  
-  include Collision
 
   WINDOW_WIDTH = 800
   WINDOW_HEIGHT = 600
 
-  @@state = :play
+  # Game state, two values are possible;
+  # :play or :gameover
+  @state = :play
 
-  # Helper method for setting up everything
-  def init_game_objects
+  def initialize
+    init_window(WINDOW_WIDTH, WINDOW_HEIGHT, 'Pong')
+    set_window_icon(load_image('assets/icon.png'))
+
+    init_audio_device
+
+    # Get the current monitor frame rate and set our target framerate to match
+    set_target_fps(get_monitor_refresh_rate(get_current_monitor))
+
+    # Setup our game objects
     @player = Paddle.new
     @rival = AI.new
     @ball = Ball.new
     @court = Court.new
   end
 
-  def initialize
-    # Start our window
-    init_window(WINDOW_WIDTH, WINDOW_HEIGHT, 'Pong')
-    set_window_icon(load_image('assets/icon.png'))
-
-    # Setup audio so we can play sounds
-    init_audio_device
-
-    # Get the current monitor frame rate and set our target framerate to match.
-    set_target_fps(get_monitor_refresh_rate(get_current_monitor))
-
-    init_game_objects
-  end
-
   def update(dt)
-    # Game logic here
-    case @@state
+    # Handle game logic based on current state
+    case @state
     when :play
-      @player.update(dt)
-      @rival.update(dt)
-      @ball.update(dt)
-
-      if @player.score == 10 || @rival.score == 10
-        @@state = :gameover
-      end
+      play(dt)
     when :gameover
-      if key_down?(KEY_R)
-        @player.reset
-        @rival.reset
-        @ball.reset
-        @@state = :play
-      end
+      gameover
     end
 
+    # Rendering logic
     drawing do
       clear
       draw
     end
+  end
+
+  def play(dt)
+    @player.update(dt)
+    @rival.update(dt)
+    @ball.update(dt)
+
+    @state = :gamover if @player.score == 10 || @rival.score == 10
+  end
+
+  def gameover
+    return nil unless key_down?(KEY_R)
+
+    @player.reset
+    @rival.reset
+    @ball.reset
+
+    @state = :play
   end
 
   def draw
@@ -74,19 +75,21 @@ module Game
     @rival.draw
     @ball.draw
 
-    if @@state == :gameover
-      draw_text("The game has ended!", 200, 200, 39, WHITE)
+    return nil unless @state == :gameover
 
-      outcome = @player.score == 10 ? 'You win!' : 'You lose!'
-      draw_text(outcome, 300, 250, 39, WHITE)
-      draw_text('Press R to restart the game', 150, 400, 39, WHITE)
-    end
+    outcome = @player.score == 10 ? 'You win!' : 'You lose!'
+
+    draw_text('The game has ended!', 200, 200, 38, WHITE)
+    draw_text(outcome, 310, 250, 39, WHITE)
+    draw_text('Press R to restart the game', 130, 550, 38, WHITE)
   end
 
+  # Loop our game until we should close the window
   def loop
     update(get_frame_time) until window_should_close?
   end
 
+  # Needed for closing resources used in taylor
   def finalize
     close_audio_device
     close_window
