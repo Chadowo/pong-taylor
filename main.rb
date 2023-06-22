23ac19dbca7f9f675aa9878f10580d6cb3c0cc58
Chadowo <83732118+Chadowo@users.noreply.github.com>
2023-06-22 12:28:12 -0300

    Added new state based logic. Removed unnecesary draw method and added VERSION.

--- main.rb
+++ main.rb
@@ -2,23 +2,18 @@
 $: << './vendor'
 $: << './src'
 
-require 'src/paddle'
-require 'src/ai'
-require 'src/ball'
-require 'src/court'
+require 'state'
+
+require 'src/states/play'
+require 'src/states/menu'
 
 # Core game module, everything happens here
 module Game
-  attr_reader :player, :rival, :ball
-
   extend self
 
   WINDOW_WIDTH = 800
   WINDOW_HEIGHT = 600
-
-  # Game state, two values are possible;
-  # :play or :gameover
-  @state = :play
+  VERSION = '1.1.0'.freeze
 
   def initialize
     init_window(WINDOW_WIDTH, WINDOW_HEIGHT, 'Pong')
@@ -29,60 +24,18 @@ module Game
     # Get the current monitor frame rate and set our target framerate to match
     set_target_fps(get_monitor_refresh_rate(get_current_monitor))
 
-    # Setup our game objects
-    @player = Paddle.new
-    @rival = AI.new
-    @ball = Ball.new
-    @court = Court.new
+    StateManager.set_state(Menu.new)
   end
 
   def update(dt)
     # Handle game logic based on current state
-    case @state
-    when :play
-      play(dt)
-    when :gameover
-      gameover
-    end
+    StateManager.update(dt)
 
     # Rendering logic
     drawing do
       clear
-      draw
+      StateManager.draw
     end
-  end
-
-  def play(dt)
-    @player.update(dt)
-    @rival.update(dt)
-    @ball.update(dt)
-
-    @state = :gameover if @player.score == 10 || @rival.score == 10
-  end
-
-  def gameover
-    return nil unless key_down?(KEY_R)
-
-    @player.reset
-    @rival.reset
-    @ball.reset
-
-    @state = :play
-  end
-
-  def draw
-    @court.draw
-    @player.draw
-    @rival.draw
-    @ball.draw
-
-    return nil unless @state == :gameover
-
-    outcome = @player.score == 10 ? 'You win!' : 'You lose!'
-
-    draw_text('The game has ended!', 200, 200, 38, WHITE)
-    draw_text(outcome, 310, 250, 39, WHITE)
-    draw_text('Press R to restart the game', 130, 550, 38, WHITE)
   end
 
   # Loop our game until we should close the window
@@ -92,8 +45,8 @@ module Game
 
   # Needed for closing resources used in taylor
   def finalize
-    close_audio_device
     close_window
+    close_audio_device
   end
 end
 
