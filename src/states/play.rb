8ce86e0604c2b7e151d75f84ef7b3c443fd4efdc
Chadowo <83732118+Chadowo@users.noreply.github.com>
2023-06-23 19:24:53 -0300

    Made functions with too much LOC(Lines Of Code) smaller, and minor tweaks

--- src/states/play.rb
+++ src/states/play.rb
@@ -28,31 +28,45 @@ class Play < StateManager::State
 
     # Determines current game flow of action
     # one of :playing,:gameover or :pause
+    # NOTE: Would it be better to use a State instead?
     @substate = :playing
   end
 
   def update(dt)
     case @substate
     when :playing
-      @player.update(dt)
-      @rival.update(dt)
-      @rival.track_ball(@ball)
-      @ball.update(dt)
-
-      @substate = :gameover if @player.score == 10 || @rival.score == 10
-      @substate = :pause if key_pressed?(KEY_ENTER)
+      playing_update(dt)
     when :pause
-      if key_pressed?(KEY_ENTER)
-        @substate = :playing
-      end
+      pause_update
     when :gameover
-      if key_pressed?(KEY_R)
-        @player.reset
-        @rival.reset
-        @ball.reset
-        @substate = :playing
-      end
+      gameover_update
     end
+  end
+
+  def playing_update(dt)
+    @player.update(dt)
+    @rival.update(dt)
+    @rival.track_ball(@ball)
+    @ball.update(dt)
+
+    @substate = :pause if key_pressed?(KEY_ENTER)
+    @substate = :gameover if @player.score == 10 || @rival.score == 10
+  end
+
+  def pause_update
+    return nil unless key_pressed?(KEY_ENTER)
+
+    @substate = :playing
+  end
+
+  def gameover_update
+    return nil unless key_pressed?(KEY_R)
+
+    @player.reset
+    @rival.reset
+    @ball.reset
+
+    @substate = :playing
   end
 
   def draw
@@ -60,7 +74,6 @@ class Play < StateManager::State
     @player.draw
     @rival.draw
     @ball.draw
-
 
     case @substate
     when :pause
@@ -71,16 +84,24 @@ class Play < StateManager::State
   end
 
   def pause_draw
+    # Transparent rectangle covering the game window to darken
+    # the screen a little
     Rectangle.new(0, 0, Game::WINDOW_WIDTH, Game::WINDOW_HEIGHT)
              .draw(colour: Colour.new(0, 0, 0, 100))
+
     draw_text('-- PAUSE --', 255, 150, 48, WHITE)
   end
 
   def gameover_draw
     outcome = @player.score == 10 ? 'You win!' : 'You lose!'
 
+    # Top text
     draw_text('The game has ended!', 200, 200, 38, WHITE)
-    draw_text(outcome, 310, 250, 39, WHITE)
+
+    # Medium text
+    draw_text(outcome, 310, 250, 38, WHITE)
+
+    # Bottom text
     draw_text('Press R to restart the game', 130, 550, 38, WHITE)
   end
 end
