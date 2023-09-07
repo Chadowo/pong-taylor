8fc8cb741e17a05b3a5485bb9d0ce0d624b0c8e5
Chadowo <83732118+Chadowo@users.noreply.github.com>
2023-09-07 14:13:01 -0300

    Remove trailing whitespace

--- lib/collision.rb
+++ lib/collision.rb
@@ -3,10 +3,10 @@ module Collision
   # AABB collision detection.
   # box1 defaults to calling object.
   def collide?(box1 = self, box2)
-    if  box1.x < box2.x + box2.w &&
-        box1.x + box1.w > box2.x &&
-        box1.y < box2.y + box2.h &&
-        box1.y + box1.h > box2.y
+    if box1.x < box2.x + box2.w &&
+       box1.x + box1.w > box2.x &&
+       box1.y < box2.y + box2.h &&
+       box1.y + box1.h > box2.y
     then
       true
     end
@@ -25,7 +25,7 @@ module Collision
       true if box.x <= 0
     when :right
       true if box.x + box.w >= Game::WINDOW_WIDTH
-    else 
+    else
       raise ArgumentError, "Border #{border} isn't a valid option"
     end
   end
